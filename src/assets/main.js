4602052f47cdba58ed5e258ecc6ba86a2fd25dc1
dakoya <david.mzt@hotmail.fr>
2017-08-20 18:46:15 +0200

    go ahead

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,54 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
+
+    if(answer.value == '' || attempt.value == '') {
+    	setHiddenFields();
+    }
+
+    if(validateInput(input.value)) {
+    	return;
+    }
+    attempt.value++;
     //add functionality to guess function here
 }
 
+function getResults(input){
+	let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+	for(i = 0; i < input.length; i++) {
+		html += '';
+	}
+}
+
 //implement new functions here
+function setHiddenFields() {
+	answer.value = Math.floor(Math.random() * 10000).toString();
+	while(answer.value.length < 4) {
+		answer.value = "0" + answer.value;
+	}
+	attempt.value= "0";
+} 
+
+function setMessage(message) {
+	document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(input) {
+	if(input.length != 4) {
+		setMessage('Guesses must be exactly 4 characters long.');
+		return false;
+	}
+	return true;
+}
+
+//tests
+let body = $('body');
+let test = document.getElementById('test');
+var test2 = $('#test');
+console.log(test);
+console.log(test2);
+
+
+
+
+
